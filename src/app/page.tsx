09020ae6eabb1a59fa396e770da271c8a913cb25
Max Noah <hsondz1910@gmail.com>
2024-06-25 00:58:03 +0700

    - Thêm css bằng .module.css - Dùng SASS module.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,19 @@
 import Link from 'next/link';
+import style1 from '@/styles/app.module.css';
+import style2 from '@/styles/phson.module.css'
 
 export default function Home() {
 	return (
     <div>
       <ul>
-        <li>
+        <li className='red'>
           {/* <a href="/about">About</a> */}
           <Link href="/about">About</Link>
         </li>
         <li>
-          <a href="/ho-so-benh">Hồ sơ bệnh</a>
+          <a className={style2['red']} href="/ho-so-benh">
+            <span className={style1['red']}>Hồ sơ bệnh</span>
+          </a>
         </li>
         <li>
           <a href="/booking">Booking</a>
