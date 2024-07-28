810b018de626d60fb55c17b8f77a152ab8328e1c
hsondz1910 <hsondz1910@gmail.com>
2024-07-28 21:57:03 +0700

    - Install the toasttify library to show the notification. - Layout: Import css, toast container (edit the config). - In the Modal file: import the toastify, add logic when take the submit button,, validate data (if valid call fetch to get data)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import AppHeader from '@/components/app.header';
 import AppFooter from '@/components/app.footer';
 import Container from 'react-bootstrap/Container';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,13 +18,24 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <AppHeader/>
+        <AppHeader />
 
-          <Container>
+        <Container>
           {children}
-          </Container>
+        </Container>
 
-        <AppFooter/>
+        <AppFooter />
+        <ToastContainer
+          position="top-center"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="light" />
       </body>
     </html>
   );
